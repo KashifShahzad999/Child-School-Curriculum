356ab4e58270d68b1ae36bced4549ef1ce4dc939
Kashif Shahzad <142479417+KashifShahzad999@users.noreply.github.com>
2024-12-07 18:43:22 +0500

    Fix missing files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./Components/Nav-Bar/NavBar";
 import Services from "./Components/Services/Services";
 import Categories from "./Components/Categories/Categorie";
 import Contact from "./Components/Contact-Us/Contact";
-import Home from "./Components/Home/Home";
+import Home from "./Components/Home/Home.jsx";
 import About from "./Components/About/About";
 function App() {
   return (
