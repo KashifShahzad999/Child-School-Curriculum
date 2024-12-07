dbb82aec4cecf50966e649608f17de1921159c42
Kashif Shahzad <142479417+KashifShahzad999@users.noreply.github.com>
2024-12-07 19:26:21 +0500

    Rename Home.jsx to HomeComponent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import NavBar from "./Components/Nav-Bar/NavBar";
 import Services from "./Components/Services/Services";
 import Categories from "./Components/Categories/Categorie";
 import Contact from "./Components/Contact-Us/Contact";
-import Home from "./Components/Home/Home.jsx";
+ import Home from "./Components/HomePage/Home"
 import About from "./Components/About/About";
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+     <Home/>
       <Services />
       <Categories />
       <About />
