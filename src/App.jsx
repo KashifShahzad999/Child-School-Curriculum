c73877496a7125df2e5eea6b17f2109ef131cbde
Kashif Shahzad <142479417+KashifShahzad999@users.noreply.github.com>
2024-12-14 02:22:09 +0500

    add team page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import NavBar from "./Components/Nav-Bar/NavBar";
 import Services from "./Components/Services/Services";
 import Categories from "./Components/Categories/Categorie";
 import Contact from "./Components/Contact-Us/Contact";
- import Home from "./Components/HomePage/Home"
+import Home from "./Components/HomePage/Home";
 import About from "./Components/About/About";
+import Teams from "./Components/Team-members/Teams";
 function App() {
   return (
     <div>
       <NavBar />
-     <Home/>
+      <Home />
       <Services />
       <Categories />
       <About />
+      <Teams />
       <Contact />
     </div>
   );
